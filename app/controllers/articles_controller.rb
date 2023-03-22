13e6c751bed6b09ef46683636388f7ac78a89c76
the-datageek <moraaf770@gmail.com>
2023-03-22 14:00:13 +0300

    Done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,11 +4,24 @@ class ArticlesController < ApplicationController
   def index
     articles = Article.all.includes(:user).order(created_at: :desc)
     render json: articles, each_serializer: ArticleListSerializer
+
+    
+    # cookies[:cookies_page_views] ||=0
+    # render json: {session: session, cookies: cookies.to_hash}
+
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+    session[:page_views] ||=0
+    session[:page_views] += 1
+
+    if session[:page_views] <= 3
+      article = Article.find(params[:id])
+      render json: article
+    else
+      render json: {error: "You have reached the maximum number of article views."}, status: :unauthorized
+    end
+    
   end
 
   private
@@ -16,5 +29,7 @@ class ArticlesController < ApplicationController
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
   end
+
+
 
 end
--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,8 @@ module Phase4CookiesAndSessionsLab
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    
+    config.api_only = true
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
